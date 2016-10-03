59deccde07efc7bb949fa9ba7046a83a6b86188a
Roger1492 <rogershen1492@gmail.com>
2016-10-03 23:06:53 +0800

    add some html and css, structure almost done, and some style has finished

--- Project/Music_Player/js/script.js
+++ Project/Music_Player/js/script.js
@@ -1,10 +1,16 @@
 var header = document.getElementById("header");
 var music = document.getElementById("music");
-var footer = document.getElementById("footer");
-var album = document.getElementById("album");
 var lyrics = document.getElementById("lyrics");
+var footer = document.getElementById("footer");
+
+music.onclick = function(){
+	this.style.zIndex = "-1";
+	lyrics.style.zIndex = "1";
+	lyrics.style.transition = "all 1s";
+}
 
 lyrics.onclick = function(){
-	this.style.display = "block";
-	album.style.display = "none";
+	this.style.zIndex = "-1";
+	music.style.zIndex = "1";
+	music.style.transition = "all 1s";
 };
